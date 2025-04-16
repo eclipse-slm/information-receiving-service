075b1eda94082a48ecd85a74632cc0859ab9cf2d
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-04-16 12:29:50 +0200

    return 404 if submodel does not exist/smId is not base64 encoded

--- routes/submodel_repo.py
+++ routes/submodel_repo.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+import binascii
+
+from fastapi import APIRouter, HTTPException
 
 from services.aas_utils import decode_id
 from services.in_memory_store.in_memory_store import InMemoryStore
@@ -19,7 +21,10 @@ submodel_handler = SubmodelHandler()
 
 @router.get(path="/submodels/{submodelIdentifier}", status_code=200, description="Returns a specific Submodel")
 def get_submodel(submodelIdentifier: str, cached: bool = True):
-    decoded_id = decode_id(submodelIdentifier)
+    try:
+        decoded_id = decode_id(submodelIdentifier)
+    except binascii.Error as e:
+        raise HTTPException(status_code=404, detail="Item not found")
 
     # if cached:
     #     submodel = get_cached_submodel(decode_id(submodelIdentifier))
@@ -27,7 +32,12 @@ def get_submodel(submodelIdentifier: str, cached: bool = True):
     #     submodel = get_remote_submodel(decode_id(submodelIdentifier))
     #
     # return json.loads(json.dumps(submodel, cls=AASToJsonEncoder))
-    return submodel_handler.get_submodel(decoded_id)
+    submodel = submodel_handler.get_submodel(decoded_id)
+
+    if submodel is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return submodel
 
 # @router.get(path="/submodels/{submodelIdentifier}/submodel-elements/{submodelElementIdentifier}", status_code=200, description="Returns a specific Submodel Element")
 # def get_submodel_element(submodelIdentifier: str, submodelElementIdentifier: str, cached: bool = True):
