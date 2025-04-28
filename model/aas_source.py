1514cdafde0da5179579f4b046576847868f9df9
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-04-28 14:52:53 +0200

    add requests for shells, shell descriptors, submodel descriptors

--- model/aas_source.py
+++ model/aas_source.py
@@ -4,7 +4,8 @@ from typing import List, Optional
 from aas_python_http_client import ApiClient, AssetAdministrationShellRegistryAPIApi, \
     AssetAdministrationShellRepositoryAPIApi, SubmodelRegistryAPIApi, GetAssetAdministrationShellDescriptorsResult, \
     AssetAdministrationShellDescriptor, AssetAdministrationShell, Submodel, SubmodelDescriptor, \
-    GetSubmodelDescriptorsResult
+    GetSubmodelDescriptorsResult, Configuration
+from aas_python_http_client.rest import ApiException
 from pydantic import BaseModel
 
 from services.aas_utils import encode_id
@@ -13,6 +14,14 @@ from services.aas_utils import encode_id
 class AasSource(BaseModel):
     polling_interval_s: int = -1
     _limit: int = 45000
+
+
+    def api_configuration(self, url: str) -> Configuration:
+        if url is None:
+            return None
+        configuration = Configuration()
+        configuration.host = url
+        return configuration
 
 
     def api_client(self) -> ApiClient:
@@ -45,6 +54,18 @@ class AasSource(BaseModel):
             return self.shell_registry_client.get_all_asset_administration_shell_descriptors(cursor=cursor, limit=limit)
 
 
+    def request_shell_descriptor(self, identifier: str) -> AssetAdministrationShellDescriptor:
+        if self.shell_registry_client is None:
+            return None
+
+        try:
+            return self.shell_registry_client.get_asset_administration_shell_descriptor_by_id(
+                encode_id(identifier)
+            )
+        except (TypeError, ApiException) as e:
+            return None
+
+
     def _request_shells(self, cursor: str, limit: int) -> dict:
         if cursor == "0":
             response = self.shell_repository_client.get_all_asset_administration_shells(
@@ -61,6 +82,29 @@ class AasSource(BaseModel):
             )
 
         return json.loads(response.data)
+
+
+    def request_shell(self, identifier: str) -> AssetAdministrationShell:
+        if self.shell_repository_client is None:
+            return None
+
+        try:
+            result = self.shell_repository_client.get_asset_administration_shell_by_id(
+                encode_id(identifier)
+            )
+
+            # Some aasx server do not return 404 but 200 with error message
+            try:
+                if any(k.lower() == "messages" for k in result.keys()):
+                    return None
+            except AttributeError:
+                pass
+
+            return result
+
+            return result
+        except (TypeError, ApiException) as e:
+            return None
 
 
     def _request_all_submodels(self) -> dict:
@@ -132,6 +176,20 @@ class AasSource(BaseModel):
         return response.result
 
 
+    def request_submodel_descriptor(self, aas_identifier: str, sm_identifier: str) -> SubmodelDescriptor:
+        if self.submodel_registry_client is None:
+            return None
+
+        try:
+            return self.shell_registry_client.get_submodel_descriptor_by_id_through_superpath(
+                encode_id(aas_identifier),
+                encode_id(sm_identifier),
+                _preload_content=False
+            )
+        except (TypeError, ApiException) as e:
+            return None
+
+
     def request_submodels(self) -> List[Submodel]:
         response = self._request_all_submodels()
         # self._shells = response.result
@@ -139,5 +197,18 @@ class AasSource(BaseModel):
 
 
     def request_submodel(self, identifier: str) -> Optional[Submodel]:
-        response = self._request_submodel(identifier)
-        return response
+        try:
+            if self.submodel_repository_client is None:
+                return None
+
+            result = self._request_submodel(identifier)
+            # Some aasx server do not return 404 but 200 with error message
+            try:
+                if any(k.lower() == "messages" for k in result.keys()):
+                    return None
+            except AttributeError:
+                pass
+
+            return result
+        except (ApiException, TypeError) as e:
+            return None
