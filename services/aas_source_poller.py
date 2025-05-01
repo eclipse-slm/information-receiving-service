5e52497c7d5d4c74518438d6dee6ba6f7405015b
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-05-01 21:36:22 +0200

    create db views on startup

--- services/aas_source_poller.py
+++ services/aas_source_poller.py
@@ -17,7 +17,8 @@ from aas.couch_db_submodel_descriptor_client import CouchDBSubmodelDescriptorCli
 from logger.logger import set_basic_config
 from model.aas_services import AasServices
 from model.aas_source import AasSource
-from services.aas_utils import get_base_url, get_base_url_shell_repo, encode_id, get_base_url_submodel_repo
+from services.aas_utils import get_base_url, get_base_url_shell_repo, encode_id, get_base_url_submodel_repo, \
+    convert_dict_keys_to_camel_case
 
 # Configure logging
 set_basic_config()
@@ -37,9 +38,18 @@ class AasSourcePoller:
         self._couchdb_shell_client = CouchDBShellClient()
         self._couchdb_submodel_descriptor_client = CouchDBSubmodelDescriptorClient(client_name=self.aas_source.name)
         self._couchdb_submodel_client = CouchDBSubmodelClient()
-        self._initiator_thread: threading.Thread = threading.Thread(target=self.do_polling)
+        self._create_db_views()
 
+        self._initiator_thread: threading.Thread = threading.Thread(target=self.do_polling)
         self._initiator_thread.start()
+
+
+    def _create_db_views(self):
+        db_clients = [self._couchdb_shell_descriptor_client, self._couchdb_shell_client, self._couchdb_submodel_descriptor_client, self._couchdb_submodel_client]
+
+        for db_client in db_clients:
+            db_client.add_source_name_as_view(self.aas_source.name.lower())
+
 
     def _specific_asset_id_serializer(self, obj):
         if isinstance(obj, SpecificAssetId) or isinstance(obj, ExternalReference):
@@ -127,7 +137,7 @@ class AasSourcePoller:
             end = time.time()
             self._log(f"Polling submodel descriptors took {end - start:.2f} seconds")
             self._log(f"Found {len(submodel_descriptors)} submodel descriptors")
-            self._couchdb_submodel_descriptor_client.save_submodel_descriptors(submodel_descriptors)
+            self._couchdb_submodel_descriptor_client.save_submodel_descriptors(self.aas_source.name, submodel_descriptors)
             return submodel_descriptors
         except (TimeoutError, ConnectTimeout) as e:
             self._log(f"TimeoutError: {e}", level=logging.ERROR)
@@ -140,7 +150,7 @@ class AasSourcePoller:
             end = time.time()
             self._log(f"Polling shells took {end - start:.2f} seconds")
             self._log(f"Found {len(shells)} shells")
-            self._couchdb_shell_client.save_shells(shells)
+            self._couchdb_shell_client.save_shells(self.aas_source.name, shells)
             return shells
         except (TimeoutError, ConnectTimeout) as e:
             self._log(f"TimeoutError: {e}", level=logging.ERROR)
@@ -154,24 +164,26 @@ class AasSourcePoller:
             elapsed_time = end - start
             self._log(f"Polling submodels took {end - start:.2f} seconds")
             self._log(f"Found {len(submodels)} submodels")
-            self._couchdb_submodel_client.save_submodels(submodels)
+            self._couchdb_submodel_client.save_submodels(self.aas_source.name, submodels)
             return submodels
         except (TimeoutError, ConnectTimeout) as e:
             self._log(f"TimeoutError: {e}", level=logging.ERROR)
             return None
 
     def write_shell_descriptors(self, shell_descriptors):
-        self._couchdb_shell_descriptor_client.save_shell_descriptors(shell_descriptors)
+        self._couchdb_shell_descriptor_client.save_shell_descriptors(self.aas_source.name, shell_descriptors)
 
 
-    def write_submodel_descriptors(self, shell_descriptors):
+    def write_submodel_descriptors(self, shell_descriptors: List[dict]):
         submodel_descriptors = []
         for shell_descriptor in shell_descriptors:
-            if shell_descriptor.submodel_descriptors is not None:
-                submodel_descriptors.extend(shell_descriptor.submodel_descriptors)
+            try:
+                submodel_descriptors.extend(shell_descriptor['submodelDescriptors'])
+            except KeyError:
+                continue
 
         self._log(f"Found {len(submodel_descriptors)} submodel descriptors")
-        self._couchdb_submodel_descriptor_client.save_submodel_descriptors(submodel_descriptors)
+        self._couchdb_submodel_descriptor_client.save_submodel_descriptors(self.aas_source.name, submodel_descriptors)
         return submodel_descriptors
 
     def stop_polling(self):
@@ -188,41 +200,42 @@ class AasSourcePoller:
         elapsed_time = end - start
         self._log(f"Adding local endpoints to descriptors took {elapsed_time:.2f} seconds")
 
-    def _endpoints_list_contains_local_endpoint(self, endpoints) -> bool:
+    def _endpoints_list_contains_local_endpoint(self, endpoints: List[dict]) -> bool:
         for endpoint in endpoints:
-            if get_base_url() in endpoint.protocol_information.href :
+            if get_base_url() in endpoint['protocolInformation']['href']:
                 return True
         return False
 
-    def _add_local_shell_endpoint(self, shell_descriptor):
-        if self._endpoints_list_contains_local_endpoint(shell_descriptor.endpoints):
+    def _add_local_shell_endpoint(self, shell_descriptor: dict):
+        if self._endpoints_list_contains_local_endpoint(shell_descriptor['endpoints']):
             return
 
-        local_shell_endpoint = Endpoint(
+        local_shell_endpoint = convert_dict_keys_to_camel_case(Endpoint(
             interface="local",
             protocol_information=ProtocolInformation(
-                href=get_base_url_shell_repo() + "/shells/" + encode_id(shell_descriptor.id)
+                href=get_base_url_shell_repo() + "/shells/" + encode_id(shell_descriptor['id'])
             )
-        )
-        shell_descriptor.endpoints.insert(0, local_shell_endpoint)
+        ).to_dict())
+        shell_descriptor['endpoints'].insert(0, local_shell_endpoint)
         # return shell_descriptor.endpoints
 
-    def _add_local_submodel_endpoints(self, shell_descriptor):
-        if shell_descriptor.submodel_descriptors is None:
+    def _add_local_submodel_endpoints(self, shell_descriptor: dict):
+        try:
+            for submodel_descriptor in shell_descriptor['submodel_descriptors']:
+                if self._endpoints_list_contains_local_endpoint(submodel_descriptor['endpoints']):
+                    continue
+                self._add_local_submodel_endpoint(submodel_descriptor)
+        except KeyError:
             return
-        for submodel_descriptor in shell_descriptor.submodel_descriptors:
-            if self._endpoints_list_contains_local_endpoint(submodel_descriptor.endpoints):
-                continue
-            self._add_local_submodel_endpoint(submodel_descriptor)
 
-    def _add_local_submodel_endpoint(self, submodel_descriptor):
-        local_submodel_endpoint = Endpoint(
+    def _add_local_submodel_endpoint(self, submodel_descriptor: dict):
+        local_submodel_endpoint = convert_dict_keys_to_camel_case(Endpoint(
             interface="local",
             protocol_information=ProtocolInformation(
-                href=get_base_url_submodel_repo() + "/submodels/" + encode_id(submodel_descriptor.id)
+                href=get_base_url_submodel_repo() + "/submodels/" + encode_id(submodel_descriptor['id'])
             )
-        )
-        submodel_descriptor.endpoints.insert(0, local_submodel_endpoint)
+        ).to_dict())
+        submodel_descriptor['endpoints'].insert(0, local_submodel_endpoint)
 
 
     def _log(self, message: str, level: int = logging.INFO):
