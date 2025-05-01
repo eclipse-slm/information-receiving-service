679d1145f230cae7d1243584c5bf9c0fef92ab2f
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-05-01 21:35:30 +0200

    return always dict on request

--- model/aas_source.py
+++ model/aas_source.py
@@ -47,11 +47,19 @@ class AasSource(BaseModel):
         pass
 
 
-    def _request_shell_descriptors(self, cursor: str, limit: int) -> GetAssetAdministrationShellDescriptorsResult:
-        if cursor == "0":
-            return self.shell_registry_client.get_all_asset_administration_shell_descriptors(limit=limit)
-        else:
-            return self.shell_registry_client.get_all_asset_administration_shell_descriptors(cursor=cursor, limit=limit)
+    def _request_shell_descriptors(self, cursor: str, limit: int) -> dict:
+        args = {
+            "limit": limit,
+            "_preload_content": False
+        }
+
+        if cursor != "0":
+            args['cursor'] = cursor
+
+        return json.loads(
+            self.shell_registry_client.get_all_asset_administration_shell_descriptors(**args).data
+        )
+            # return self.shell_registry_client.get_all_asset_administration_shell_descriptors(cursor=cursor, limit=limit)
 
 
     def request_shell_descriptor(self, identifier: str) -> AssetAdministrationShellDescriptor:
@@ -67,19 +75,15 @@ class AasSource(BaseModel):
 
 
     def _request_shells(self, cursor: str, limit: int) -> dict:
-        if cursor == "0":
-            response = self.shell_repository_client.get_all_asset_administration_shells(
-                limit=limit,
-                _preload_content=False,
-                _return_http_data_only=True
-            )
-        else:
-            response = self.shell_repository_client.get_all_asset_administration_shells(
-                cursor=cursor,
-                limit=limit,
-                _preload_content=False,
-                _return_http_data_only=True
-            )
+        args = {
+            "limit": limit,
+            "_preload_content": False
+        }
+
+        if cursor != "0":
+            args['cursor'] = cursor
+
+        response = self.shell_repository_client.get_all_asset_administration_shells(**args)
 
         return json.loads(response.data)
 
@@ -129,24 +133,18 @@ class AasSource(BaseModel):
 
 
     def _request_submodels(self, cursor: str, limit: int) -> dict:
-        if cursor == "0":
-            response = self.submodel_repository_client.get_all_submodels(
-                limit=limit,
-                _preload_content=False,
-                _return_http_data_only=True
-            )
-        else:
-            response = self.submodel_repository_client.get_all_submodels(
-                cursor=cursor,
-                limit=limit,
-                _preload_content=False,
-                _return_http_data_only=True
-            )
+        args = {
+            "limit": limit,
+            "_preload_content": False
+        }
+        if cursor != "0":
+            args['cursor'] = cursor
 
+        response = self.submodel_repository_client.get_all_submodels(**args)
         return json.loads(response.data)
 
 
-    def _request_submodel(self, identifier: str):
+    def _request_submodel(self, identifier: str) -> dict:
         response = self.submodel_repository_client.get_submodel_by_id(
             submodel_identifier=encode_id(identifier),
             _preload_content=False,
@@ -154,20 +152,26 @@ class AasSource(BaseModel):
         )
         return json.loads(response.data)
 
-    def _request_submodel_descriptors(self, cursor: str, limit: int) -> GetSubmodelDescriptorsResult:
-        if cursor == "0":
-            return self.submodel_registry_client.get_all_submodel_descriptors(limit=limit)
-        else:
-            return self.submodel_registry_client.get_all_submodel_descriptors(cursor=cursor, limit=limit)
+    def _request_submodel_descriptors(self, cursor: str, limit: int) -> dict:
+        args = {
+            "limit": limit,
+            "_preload_content": False
+        }
+
+        if cursor != "0":
+            args['cursor'] = cursor
+
+        return json.loads(
+            self.submodel_registry_client.get_all_submodel_descriptors(**args).data
+        )
 
 
-    def request_shell_descriptors(self) -> List[AssetAdministrationShellDescriptor]:
+    def request_shell_descriptors(self) -> List[dict]:
         response = self._request_shell_descriptors(str(0), self._limit)
-        return response.result
+        return response['result']
 
-    def request_shells(self) -> List[AssetAdministrationShell]:
+    def request_shells(self) -> List[dict]:
         response = self._request_shells(str(0), self._limit)
-        # self._shells = response.result
         return response['result']
 
 
