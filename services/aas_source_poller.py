7b89937ead0205c1a613c66786cb8c79bfd17f1f
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2025-06-23 18:25:36 +0200

    fix typo

--- services/aas_source_poller.py
+++ services/aas_source_poller.py
@@ -277,7 +277,7 @@ class AasSourcePoller:
                     {
                         'interface': 'local',
                         'protocolInformation': {
-                            'href': get_base_url_submodel_repo() + "/submodels/" + encode_id(submodel_id)
+                            'href': get_base_url_submodel_repo() + "/submodels/" + encode_id(submodel_id),
                             'endpointProtocol': 'http'
                         }
                     }
