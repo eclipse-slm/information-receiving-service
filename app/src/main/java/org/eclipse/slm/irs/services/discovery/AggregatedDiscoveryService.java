b8d298fc8b68b18cf4a50b0b4df6e3bedccd7cb7
Matthias Schneider <matthias.schneider@ipa.fraunhofer.de>
2026-05-20 15:54:40 +0200

    Improve error handling

--- app/src/main/java/org/eclipse/slm/irs/services/discovery/AggregatedDiscoveryService.java
+++ app/src/main/java/org/eclipse/slm/irs/services/discovery/AggregatedDiscoveryService.java
@@ -1,9 +1,14 @@
 package org.eclipse.slm.irs.services.discovery;
 
+import org.eclipse.slm.aas.clients.base.FeignResponseException;
 import org.eclipse.slm.aas.model.discovery.AssetLink;
+import org.eclipse.slm.aas.model.discovery.expcetions.DiscoveryClientRuntimeException;
 import org.eclipse.slm.irs.clients.aas.DiscoveryClientFactory;
 import org.eclipse.slm.irs.config.AasServersConfig;
+import org.eclipse.slm.irs.config.ConfigLoader;
 import org.eclipse.slm.irs.utils.Base64Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,6 +18,8 @@ import java.util.List;
 
 @Component
 public class AggregatedDiscoveryService implements DiscoveryService {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AggregatedDiscoveryService.class);
 
     private final AasServersConfig aasServersConfig;
 
@@ -33,11 +40,29 @@ public class AggregatedDiscoveryService implements DiscoveryService {
             try {
                 var aasIds = discoveryClient.getAllAssetAdministrationShellIdsByAssetId(assetIdBase64Encoded);
                 allAasIds.addAll(Arrays.stream(aasIds).toList());
-            } catch (Exception e) {
-                var assetId = Base64Util.decodeFromBase64(assetIdBase64Encoded);
-                var assetLink = new AssetLink().name("globalAssetId").value(assetId);
-                var aasIds = discoveryClient.getAllAssetAdministrationShellIdsByAssetLink(assetLink);
-                allAasIds.addAll(Arrays.stream(aasIds).toList());
+            }
+            catch (Exception e) {
+                if (e instanceof FeignResponseException) {
+                    var fe = (FeignResponseException) e;
+                    if (fe.getStatusCode() == 401) {
+                        LOG.error("Failed to authenticate at AAS server '{}'", aasServerConfig.getName());
+                    } else {
+                        LOG.error("Failed to get AAS IDs for asset ID '{}' from AAS server '{}': {}", assetIdBase64Encoded, aasServerConfig.getName(), fe.getMessage(), fe);
+                    }
+                }
+                else if (e.getMessage().contains("timed out")) {
+                    LOG.error("Connection to AAS server '{}' timed out", aasServerConfig.getName());
+                }
+                else {
+                    try {
+                        var assetId = Base64Util.decodeFromBase64(assetIdBase64Encoded);
+                        var assetLink = new AssetLink().name("globalAssetId").value(assetId);
+                        var aasIds = discoveryClient.getAllAssetAdministrationShellIdsByAssetLink(assetLink);
+                        allAasIds.addAll(Arrays.stream(aasIds).toList());
+                    } catch (Exception e2) {
+                        LOG.error("Failed to get AAS IDs for asset ID '{}' from AAS server '{}': {}", assetIdBase64Encoded, aasServerConfig.getName(), e2.getMessage(), e2);
+                    }
+                }
             }
         }
 
